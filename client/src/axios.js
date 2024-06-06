f4c80c05e40cda65c02bdf073b39834151d398df
Shivam Bansal <shivamsb2003@gmail.com>
2024-06-06 12:27:46 +0530

    fixed url

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({ baseURL: "http://localhost:3000" });
+const axiosInstance = axios.create({
+  baseURL: "https://style-sphere-api.vercel.app",
+});
 
 export default axiosInstance;
